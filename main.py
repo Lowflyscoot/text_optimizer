a471ca2cf7f119ddcd4e5107b7593b63dda3bae0
DMITRY <d.stasenko@mirtekgroup.ru>
2023-08-02 22:34:03 +0300

    Доработка чтобы отображались слова по тексту и число символов/слов

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class Ui(QtWidgets.QMainWindow):
         self.show()
 
         self.keywords = {}
+        self.words = {}
         self.calculate_processing = False
 
         self.userTextEdit.textChanged.connect(self.calculate_keywords)
@@ -23,11 +24,49 @@ class Ui(QtWidgets.QMainWindow):
 
     def calculate_keywords(self):
         if self.calculate_processing:
-            seo_text = self.userTextEdit.toPlainText()
-            responce_text = ""
+            # берём текст из левого поля
+            seo_text = self.userTextEdit.toPlainText().lower()
+            responce_text = "required keys:\n"
+            # выводим ключевики, вычитая уже введённые слова в счётчик
             for keyword, frequency in self.keywords.items():
                 responce_text += f"{keyword} {int(frequency) - seo_text.count(keyword)}\n"
+
+            responce_text += "\nfacted keys:\n"
+            self.calculate_words_frequency(seo_text)
+            counter = 0
+            for word, freq in self.words.items():
+                responce_text += f"{word}: {freq}\n"
+                counter += 1
+                if counter > 10:
+                    break
+
+            responce_text += "\n" + self.calculate_text_parameters(seo_text)
             self.systemTextEdit.setText(responce_text)
+
+    def calculate_text_parameters(self, input):
+        number_of_words = len(input.split())
+        number_of_chars = len(input.replace(" ", ""))
+        return f"words: {number_of_words}, char without spaces: {number_of_chars}"
+
+    def calculate_words_frequency(self, input):
+        text_buf = input.replace(",", "")
+        text_buf = text_buf.replace(".", "")
+        text_buf = text_buf.split()
+        self.words = {}
+        tmp_words = {}
+        for word in text_buf:
+            if word in self.words.keys():
+                self.words[word] += 1
+            else:
+                self.words.update({word: 1})
+
+        sorted_keys = sorted(self.words, key=self.words.get, reverse=True)
+
+        for key in sorted_keys:
+            if len(key) > 4:
+                tmp_words.update({key: self.words[key]})
+
+        self.words = tmp_words
 
     def start_calculating(self):
         if not self.calculate_processing:
@@ -37,6 +76,7 @@ class Ui(QtWidgets.QMainWindow):
             self.userTextEdit.setText("")
 
     def input_parsing(self, input):
+        input = input.lower()
         for pare in input.split("\n"):
             keyword, frequency = pare.split()
             keyword = keyword.replace("_", " ")
